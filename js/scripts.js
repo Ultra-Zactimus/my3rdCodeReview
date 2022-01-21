9a178043e5a0e38d14a87b685cde4301d71bab78
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 09:32:20 -0800

    work in progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 function mrRoboto() {
-  if (0){
-    
-  } 
-  return 0;
+  const roboLingo = 'beep';
+  if (0) {
+    return roboLingo;
+  // } else if (mrRoboto(1)) {
+  //     roboLingo[1];
+  // }
+}
 }
 mrRoboto(0);
