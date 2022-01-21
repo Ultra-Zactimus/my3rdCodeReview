5295b5d29f25b775085b7e86339fd789869cb554
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 09:49:05 -0800

    add new function, it returns 'boop'

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,12 @@
 function mrRoboto(input) {
   const roboLingo = 'beep';
+  const roboLingua = 'boop';
   if (input === 0) {
     return '0';
   } else if (input === 1) {
     return roboLingo;
+  } else if (input === 2) {
+    return roboLingua;
   }
 }
-mrRoboto(0);
+mrRoboto(2);
