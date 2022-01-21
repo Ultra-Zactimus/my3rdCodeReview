050d5abe0c6bb318b6d68e1f2cd5378e040df7bd
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 09:58:18 -0800

    add new function. test returns roboSwag

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 function mrRoboto(input) {
   const roboLingo = 'beep';
   const roboLingua = 'boop';
+  const roboSwag = "Won't you be my neighbor?";
   if (input === 0) {
     return '0';
   } else if (input === 1) {
     return roboLingo;
   } else if (input === 2) {
     return roboLingua;
+  } else if (input === 3) {
+    return roboSwag;
   }
 }
-mrRoboto(2);
+mrRoboto();
