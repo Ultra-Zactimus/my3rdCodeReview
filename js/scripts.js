224ef9a11ec58ea25a9e9e80edbd12bea6cc336f
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 10:15:45 -0800

    add change to function, returns values from array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
+// function mrRoboto(input) {
+//   const roboLingo = 'beep';
+//   const roboLingua = 'boop';
+//   const roboSwag = "Won't you be my neighbor?";
+//   if (input === 0) {
+//     return '0';
+//   } else if (input === 1) {
+//     return roboLingo;
+//   } else if (input === 2) {
+//     return roboLingua;
+//   } else if (input === 3) {
+//     return roboSwag;
+//   }
+// }
+// mrRoboto();
+
 function mrRoboto(input) {
-  const roboLingo = 'beep';
-  const roboLingua = 'boop';
-  const roboSwag = "Won't you be my neighbor?";
+  const roboKotoba = ['0', 'beep', 'boop', "Won't you be my neighbor?"];
   if (input === 0) {
-    return '0';
+    return roboKotoba[0];
   } else if (input === 1) {
-    return roboLingo;
+    return roboKotoba[1];
   } else if (input === 2) {
-    return roboLingua;
+    return roboKotoba[2];
   } else if (input === 3) {
-    return roboSwag;
+    return roboKotoba[3];
   }
 }
 mrRoboto();
