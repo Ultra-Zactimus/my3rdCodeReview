1ee9fc2138c147e5366994eda5d511640b534d22
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 11:30:31 -0800

    add change to function, returns 1 even if input 0 and less than 2

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ function mrRoboto(input) {
   let roboKotoba = ['0', 'beep', 'boop', "Won't you be my neighbor?"];
   if (input === 0) {
     return roboKotoba[0];
-  } else if (input === 1) {
+  } else if (input > 0 && input < 2 || input === 1) {
     return roboKotoba[1];
   } else if (input === 2) {
     return roboKotoba[2];
