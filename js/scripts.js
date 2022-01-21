e90d917f4ad6b3caff4e624bfd7fe2d16e8fda43
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 13:50:01 -0800

    add change to function, input 1+ = 'beep'

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 function mrRoboto(input) {
+
+  let userInput = input.toString();
   let roboKotoba = ['0', 'beep', 'boop', "Won't you be my neighbor?"];
-  if (input === 0) {
-    return roboKotoba[0];
-  } else if (input > 0 && input < 2 || input === 1) {
-    return roboKotoba[1];
-  } else if (input === 2) {
-    return roboKotoba[2];
-  } else if (input === 3) {
-    return roboKotoba[3];
-  } else if (input === 5) {
-    roboKotoba = [0, 'beep', 'boop', "Won't you be my neighbor?", 4, 5];
-    let roboOboete = roboKotoba.join(", ");
-    return roboOboete;
-  }
+
+    
+
+    if (userInput.includes(0)) {
+      return roboKotoba[0];
+
+    } else if (userInput.includes(1) && !userInput.includes(2 || 3 || 4 || 5 || 6 || 7 || 8 || 9)) {
+      return roboKotoba[1];
+
+    } else if (userInput.includes(2) && !userInput.includes(3 || 4 || 5 || 6 || 7 || 8 || 9)) {
+      return roboKotoba[2];
+
+    } else if (userInput.includes(3) && !userInput.includes(5 || 6 || 7 || 8 || 9)) {
+      return roboKotoba[3];
+
+    } else if (userInput.includes(5) && !userInput.includes(6 || 7 || 8 || 9)) {
+      roboKotoba = [0, 'beep', 'boop', "Won't you be my neighbor?", 4, 5];
+      let roboOboete = roboKotoba.join(", ");
+      return roboOboete;
+
+    }
 }
-mrRoboto();
+
+mrRoboto(11);
