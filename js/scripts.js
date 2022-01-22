98f9eb9715d5f82c1d1589fdbe6bf7ff939501df
Ultra Zacky <ultrazacky@gmail.com>
2022-01-21 16:37:40 -0800

    add for loop to try to fix problem. work-in-progress

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,45 @@
 function mrRoboto(input) {
-    let userInput = input.toString();
+    let inputArray = input.split("");
     let roboKotoba = ['0', 'beep', 'boop', "Won't you be my neighbor?"];
+      for (i=0; i < inputArray.length; i++)
 
-        if (userInput.includes(0)) {
-          return 0;
+        if (inputArray.includes(0)) {
+          return roboKotoba[0];
     
-        } else if (userInput.includes(1) && userInput.includes(1) > 0) {
+        } else if (inputArray.includes(1)) {
           return roboKotoba[1];
     
-        } else if (userInput.includes(2) && userInput.includes(2) > 0) {
+        } else if (inputArray.includes(2)) {
           return roboKotoba[2];
     
-        } else if (userInput.includes(3)) {
+        } else if (inputArray.includes(3)) {
           return roboKotoba[3];
     
-        } else if (userInput.includes(4)) {
+        } else if (inputArray.includes(4)) {
           roboKotoba.push(4)
           return roboKotoba[4];
     
-        } else if (userInput.includes(5)) {
+        } else if (inputArray.includes(5)) {
           roboKotoba.push(4,5);
           let roboOboete = roboKotoba.join(", ");
           return roboOboete;
     
-        } else if (userInput.includes(6)) {
+        } else if (inputArray.includes(6)) {
           roboKotoba.push(4,5,6);
           let roboOboete = roboKotoba.join(", ");
           return roboOboete;
     
-        } else if (userInput.includes(7)) {
+        } else if (inputArray.includes(7)) {
           roboKotoba.push(4,5,6,7);
           let roboOboete = roboKotoba.join(", ");
           return roboOboete;
     
-        } else if (userInput.includes(8)) {
+        } else if (inputArray.includes(8)) {
           roboKotoba.push(4,5,6,7,8);
           let roboOboete = roboKotoba.join(", ");
           return roboOboete;
     
-        } else if (userInput.includes(9)) {
+        } else if (inputArray.includes(9)) {
           roboKotoba.push(4,5,6,7,8,9);
           let roboOboete = roboKotoba.join(", ");
           return roboOboete;
@@ -46,4 +47,3 @@ function mrRoboto(input) {
         }
     }
     
-    mrRoboto(0);
