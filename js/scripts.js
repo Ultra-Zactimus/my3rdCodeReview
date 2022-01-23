50c31b057c30a49488eb74bf277d5788778824f3
Ultra Zacky <ultrazacky@gmail.com>
2022-01-23 13:40:13 -0800

    return code back to original. nest does not work

--- js/scripts.js
+++ js/scripts.js
@@ -5,127 +5,69 @@ function mrRoboto() {
   let robofying = converting.split("");
   let kotobaArray = ['0', 'beep', 'boop', "Won't you be my neighbor?", 4, 5];
 
-  for (let i = 0; i < robofying.length; i++) {
+    for(let i = 0; i < robofying.length; i++) {
 
-    if (robofying[i].includes(9)) {
-      kotobaArray.push(6, 7, 8, 9);
+    if (robofying[i].includes(0)) {
+      return alert(kotobaArray[0]);
+    
+    } else if (robofying[i].includes(1)) {
+      return alert(kotobaArray[1]);
+    
+    } else if (robofying[i].includes(2)) {
+      return alert(kotobaArray[2]);
+
+    } else if (robofying[i].includes(3)) {
+      return alert(kotobaArray[3]);
+
+    } else if (robofying[i].includes(4)) {
+      return alert(kotobaArray[4]);
+     
+    } else if (robofying[i].includes(5)) {
+      let roboKoto = kotobaArray.join(", ");
+      return alert(roboKoto);
+
+    } else if (robofying[i].includes(6)) {
+      kotobaArray.push(6);
+      let roboKoto = kotobaArray.join(", ");
+      return alert(roboKoto);
+
+    } else if (robofying[i].includes(7)) {
+      kotobaArray.push(6,7);
+      let roboKoto = kotobaArray.join(", ");
+      return alert(roboKoto);
+
+    } else if (robofying[i].includes(8)) {
+      kotobaArray.push(6,7,8);
+      let roboKoto = kotobaArray.join(", ");
+      return alert(roboKoto);
+
+    } else if (robofying[i].includes(9)) {
+      kotobaArray.push(6,7,8,9);
       let roboKoto = kotobaArray.join(", ");
       return alert(roboKoto);
 
     } else {
-
-      for (let i = 0; i < robofying.length; i++) {
-
-        if (robofying[i].includes(8)) {
-          kotobaArray.push(6, 7, 8);
-          let roboKoto = kotobaArray.join(", ");
-          return alert(roboKoto);
-
-        } else {
-
-          for (let i = 0; i < robofying.length; i++) {
-
-            if (robofying[i].includes(7)) {
-              kotobaArray.push(6, 7);
-              let roboKoto = kotobaArray.join(", ");
-              return alert(roboKoto);
-
-            } else {
-
-              for (let i = 0; i < robofying.length; i++) {
-
-                if (robofying[i].includes(6)) {
-                  kotobaArray.push(6);
-                  let roboKoto = kotobaArray.join(", ");
-                  return alert(roboKoto);
-
-                } else {
-
-                  for (let i = 0; i < robofying.length; i++) {
-
-                    if (robofying[i].includes(5)) {
-                      let roboKoto = kotobaArray.join(", ");
-                      return alert(roboKoto);
-
-                    } else {
-
-                      for (let i = 0; i < robofying.length; i++) {
-
-                        if (robofying[i].includes(4)) {
-                          return alert(kotobaArray[4]);
-
-                        } else {
-
-                          for (let i = 0; i < robofying.length; i++) {
-
-                            if (robofying[i].includes(3)) {
-                              return alert(kotobaArray[3]);
-
-                            } else {
-
-                              for (let i = 0; i < robofying.length; i++) {
-
-                                if (robofying[i].includes(2)) {
-                                  return alert(kotobaArray[2]);
-
-                                } else {
-
-                                  for (let i = 0; i < robofying.length; i++) {
-
-                                    if (robofying[i].includes(1)) {
-                                      return alert(kotobaArray[1]);
-
-                                    } else {
-
-                                      for (let i = 0; i < robofying.length; i++) {
-
-                                        if (robofying[i].includes(0)) {
-                                          return alert(kotobaArray[0]);
-
-                                        } else {
-
-                                          return alert("Mr. Robogers: \"You have entered an invalid Input! BEEP! BOOP!\nONLY UNDERSTAND NUMBERS!\n TrY aGa1n N3iGhBoR! \"");
-
-                                        }
-                                      }
-                                    }
-                                  }
-                                }
-                              }
-                            }
-                          }
-
-                        }
-                      }
-
-                    }
-
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+      return alert("Mr. Robogers: \"You have entered an invalid Input! BEEP! BOOP!\nONLY UNDERSTAND NUMBERS!\n TrY aGa1n N3iGhBoR! \"");
+    
+    }   
   }
 }
 
 
-                function resetInput() {
-                  robofying.reset();
-                }
+function resetInput() {
+  robofying.reset();
+}
 
 
-                $(document).ready(function() {
+$(document).ready(function() {
 
-                  $("form#roboNum").submit(function(event) {
+  $("form#roboNum").submit(function(event) {
 
-                    mrRoboto();
-                    resetInput();
+    mrRoboto();
+    resetInput();
 
-                    event.preventDefault();
+    event.preventDefault();
 
-                  });
+  });
 
-                });
+});
